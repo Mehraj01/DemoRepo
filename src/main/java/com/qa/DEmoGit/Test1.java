3b2beb681b963a9a2f92d3af7a4bc7599fbba5f4
mehrajismayilov <mhrj33@gmail.com>
2019-09-30 23:54:18 -0400

    firs commit.....

---
+++ src/main/java/com/qa/DEmoGit/Test1.java
@@ -0,0 +1,13 @@
+package com.qa.DEmoGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class Test1 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
